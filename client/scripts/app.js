68830dfba0ad3dc9ce5ca42d29a719ec394bde25
James Kolonusz <jamesak@gmail.com>
2015-01-05 15:51:57 -0800

    Fix backbone and default errors.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,9 @@
 var Message = Backbone.Model.extend({
   url: 'https://api.parse.com/1/classes/chatterbox/',
   defaults: {
-    username: ''
+    username: '',
+    text: '',
+    objectId: ''
   }
 });
 
